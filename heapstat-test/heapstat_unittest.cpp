6ae2ee4c65fb53847d7570cbad6a690ef76e8546
Kazuyoshi Aizawa <admin2@whiteboard.ne.jp>
2014-05-05 18:54:55 +0900

    mod: move inc directory to include

--- heapstat-test/heapstat_unittest.cpp
+++ heapstat-test/heapstat_unittest.cpp
@@ -1,5 +1,28 @@
 #include "gtest/gtest.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include "HeapStat.h"
+#include "ProcService.h"
 
 TEST(BasicTest, SimpleTest) {
+
+    int pid = fork();
+    if (pid == 0)
+    {
+        char command[] = "leak";
+        system(command);
+        exit(0);
+    }
+
+    ProcService procService(path, debug, verbose);
+    HeapStat heapStat(procService, debug, verbose);
+
+    while (true) {
+        if (verbose) {
+            procService.print_process_info();
+        }
+        heapStat.print_heap_usage();        
+    
+    
     EXPECT_EQ(1, 1);
 }
